14251df9ddf4d6fc27a95205543c6c9d73607846
Louis PLANES <Louis.PLANES@thalesgroup.com>
2025-12-08 19:58:42 +0100

    Initial commit day 1-4

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::io;
+use anyhow::Result;
+mod day1;
+mod day2;
+mod day3;
+mod day4;
+
+
+fn main() -> Result<()> {
+        println!("choose a day, 0 to quit:");
+        let mut choice = String::new();
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("error while reading choice");
+
+        let choice :u32 = choice.trim().parse()?;
+
+        match choice {
+            1 => Ok(day1::part1()?),
+            2 => Ok(day2::part1()?),
+            3 => Ok(day3::part1()?),
+            4 => Ok(day4::part1()?),
+            _ => Ok(println!("Unknown day!")),
+    }
+}
